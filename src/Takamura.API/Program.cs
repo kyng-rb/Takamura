719088243d028fcdc13a749b38342028e923151f
Reynaldo <rodriguezreynaldo1698@outlook.com>
2025-10-04 00:03:35 -0600

    Added scalar documentation support

--- src/Takamura.API/Program.cs
+++ src/Takamura.API/Program.cs
@@ -1,19 +1,8 @@
-using Takamura.API.Endpoints;
-using Takamura.Application;
+using Takamura.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var databaseConnectionString = builder.Configuration.GetConnectionString("TakamuraConnectionString");
-
-builder.Services.AddDatabaseContext(databaseConnectionString!);
-builder.Services.AddServices();
-
-var app = builder.Build();
-app.UseHttpsRedirection();
-
-app.MapGet("/", () => "Takamura API is running...");
-
-CategoryEndpoints.Map(app);
-BudgetEndpoints.Map(app);
+builder.ConfigureServices();
+var app = builder.ConfigureAPI();
 
 app.Run();
